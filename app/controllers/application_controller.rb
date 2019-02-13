d04e844f73d6e1eb86c9bbf8d8d240879955afdc
notryanb@gmail.com <notryanb@gmail.com>
2019-02-12 19:14:56 -0500

    add simple authentication to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def authenticate
+    if current_user
+      true
+    else
+      redirect_to :new_user_session
+    end
+  end
 end
