e0206909d7d25e08938269db6b7919885372529a
notryanb@gmail.com <notryanb@gmail.com>
2019-02-13 21:04:56 -0500

    update routes with root and actions for other models

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  root "maintenance_requests#index"
+  resources :maintenance_requests
+  resources :request_statuses
+  resources :request_categories
   resources :user_sessions, only: [:new, :create]
   get "/logout" => "user_sessions#destroy", as: :logout
 end
