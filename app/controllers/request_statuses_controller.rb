1458eb2eb7d769f3224dc2fa66fc0f16a47e1ea1
notryanb@gmail.com <notryanb@gmail.com>
2019-02-13 21:04:38 -0500

    add controllers for categories and statuses

---
+++ app/controllers/request_statuses_controller.rb
@@ -0,0 +1,20 @@
+class RequestStatusesController < ApplicationController
+  def index
+    @request_statuses = RequestStatus.all
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
