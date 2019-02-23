937608d8b351cde4b9fd9c6e42985d3c81470e2d
notryanb@gmail.com <notryanb@gmail.com>
2019-02-23 08:46:31 -0500

    Add errors to resident form

--- app/models/resident.rb
+++ app/models/resident.rb
@@ -2,4 +2,8 @@ class Resident < ApplicationRecord
   has_many :maintenance_requests
 
   validates_presence_of :first_name, :last_name, :address, :email
+
+  def full_name
+    [first_name, last_name].join(" ")
+  end
 end
